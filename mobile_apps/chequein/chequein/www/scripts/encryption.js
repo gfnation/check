f4ebfdc1dca0950539c831ef05341820e11ba2d4
gfnation <gfyankeesfan@aol.com>
2017-04-20 18:11:12 -0400

    .

---
+++ mobile_apps/chequein/chequein/www/scripts/encryption.js
@@ -0,0 +1,12 @@
+﻿var mysql = require("mysql");
+
+
+//Will take on non-encripted message and encrypt it
+function encrypt(message) {
+    var x = 5;
+    for (i = 0; i < message.length; i++){
+        message = setCharAt(message, i, message.fromCharCode(message.charCodeAt(i) + x));
+    }
+    
+    return message;
+}
