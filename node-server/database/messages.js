39420cea6854816f850d17bac9879b9f14adfcae
Kevin Chan <kevin.chan.dev@gmail.com>
2017-04-22 23:37:47 -0400

    API Update
    -Get Tests can be found in the “test-request.txt” file in the root
    directory.
    -Majority of API is complete.
    -Invitation system is incomplete.
    -Let me know if there are any bugs
    -Let me know if you are having any problems with the api.

--- node-server/database/messages.js
+++ node-server/database/messages.js
@@ -1,45 +1,48 @@
 var mysql = require("mysql");
-var util = require("..utils/util.js");
+var util = require("../utils/util.js");
 
+/*
+	Loads all the messages associated with a conversation_id
+*/
 exports.load_messages = function(connection, conversation_id){
 	return new Promise(function(resolve, reject){
 		var query = "SELECT * FROM tb_messages WHERE conversation_id=? ORDER BY message_time ASC";
-		connection.query(query, [username], function (err, rows, fields){
+		var array = [];
+		connection.query(query, [conversation_id], function (err, rows, fields){
 			if (err) {
 				reject(err);
 			} else {
-				var array = [];
-
 				rows.forEach(function(element){
 					var source_id = element['source_id'];
 					var message_time = element['message_time'];
 					var encrypted_message = element['message'];
 					var decrypted_message = decrypt(encrypted_message);
-					var element = {'message' : decrypted_message, 'source_id' : source_id, 'message_time' : message_time};
-					array.push(element);
+
+					var message_entry = {'message' : decrypted_message, 'source_id' : source_id, 'message_time' : message_time};
+					array.push(message_entry);
 				});
-				
 				resolve(array);
 			}
 		});
 	});
 };
 
+/*
+	Stores a message given a coversation_id, a sender_id, and the raw message
+*/
 exports.save_message = function(connection, conversation_id, source_id, raw_message){
 	return new Promise(function(resolve, reject){
 		var query = "INSERT INTO tb_messages (message_id, conversation_id, source_id, message) VALUES(?, ?, ?, ?)";
 
-		var message_id = util.md5_timed(message + source_id);
+		var message_id = util.md5_timed(raw_message + source_id);
 		var encrypted_message = encrypt(raw_message);
 
 		connection.query(query, [message_id, conversation_id, source_id, encrypted_message], function (err, rows, fields){
 			if (err) {
 				reject(err);
-			} else {
-				resolve(message_id);
 			}
+			resolve(message_id);
 		});
-		resolve(undefined);
 	});
 };
 
--- node-server/database/users.js
+++ node-server/database/users.js
@@ -1,20 +1,51 @@
 var mysql = require("mysql");
 
-exports.get_visible = function(connection, user_id){
-	return exports.get_type(connection, user_id).then(function(result){
+/*
+	Returns ALL users
+*/
+exports.get_all_users = function(connection){
+	return new Promise(function(resolve, reject){
 		var array = [];
-		var user_type = result;
+		var query = "SELECT * FROM tb_users";
+		connection.query(query, function(err, rows){
+			if (err) {
+				reject(err);
+			} else {
+				rows.forEach(function(element){
+					var u_id = element['user_id'];
+					var d_name = element['display_name'];
+					var acc_type = element['account_type'];
+					var user_info = {user_id : u_id, display_name: d_name, account_type: acc_type};
+					array.push(user_info);
+				});
+			}
+			resolve(array);
+		});
+	});
+}
 
-		var query = "SELECT user_id FROM tb_users WHERE user_type= AND user_id<>?";
+/*
+	Returns all users visible to the target user
+*/
+exports.get_visible = function(connection, user_id, account_type){
+	return new Promise(function(resolve, reject){
+		var array = [];
 
-		if (user_type == 0) {
-
-		} else if (user_type == 1) {
-
-		} else if (user_type == 2) {
-			
+		if (account_type == 0 || account_type == 1) {
+			get_admins(connection, user_id).then(function(result){
+				result.forEach(function(element){
+					array.push(element);
+				})
+				return get_associates(connection, user_id);
+			}).then(function(result){
+				result.forEach(function(element){
+					array.push(element);
+				});
+				resolve(array);
+			}).catch(function(reject){
+				throw reject;
+			});
 		}
-
 	});
 };
 
@@ -29,46 +60,95 @@ exports.get_type = function(connection, user_id){
 		var query = "SELECT * FROM tb_users WHERE user_id=?";
 		connection.query(query, [user_id], function(err, rows, fields){
 			if (err) {
-				return err;
+				reject(err);
 			} else {
 				if (rows.length > 0) {
 					var type = row['user_type'];
-					return type;
+					resolve(type);
+				} else {
+					resolve(-1);
 				}
 			}
 		});
-		return undefined;
 	});
 };
 
-exports.get_display_names = function(connection, user_id){
+/*
+	Returns the display name of the user
+*/
+exports.get_display_name = function(connection, user_id){
 	return new Promise(function(resolve, reject){
-		var query "SELECT * FROM tb_users WHERE user_id=?";
+		var query = "SELECT * FROM tb_users WHERE user_id=?";
 		connection.query(query, [user_id], function(err, rows, fields){
 			if (err) {
-				return err;
+				 reject(err);
 			} else {
 				if (rows.length > 0) {
-					return rows[0]['display_name'];
+					resolve(rows[0]['display_name']);
+				} else {
+					resolve("DISPLAY NAME ERROR");
 				}
 			}
 		});
 	});
 };
 
+/*
+	Returns all associate accounts that is not the target user	
+*/
 function get_associates(connection, user_id){
 	return new Promise(function(resolve, reject){
+		var array = [];
 		var query = "SELECT * FROM tb_users WHERE user_id<>? && user_type=?"
 		connection.query(query, [user_id, 1], function(err, rows, fields){
 			if (err) {
-				throw err;
+				reject(err);
 			} else {
-
+				rows.forEach(function(element){
+					var u_id = element['user_id'];
+					var d_name = element['display_name'];
+					var acc_type = element['account_type'];
+					var user_info = {user_id : u_id, display_name: d_name, account_type: acc_type};
+					array.push(user_info);
+				});
 			}
+			resolve(array);
 		});
 	});
 }
 
+/*
+	Returns all admins that is not the target user
+*/
 function get_admins(connection, user_id){
+	return new Promise(function(resolve, reject){
+		var array = [];
+		var query = "SELECT * FROM tb_users WHERE user_id<>? && user_type=?"
+		connection.query(query, [user_id, 0], function(err, rows, fields){
+			if (err) {
+				reject(err);
+			} else {
+				rows.forEach(function(element){
+					var u_id = element['user_id'];
+					var d_name = element['display_name'];
+					var acc_type = element['account_type'];
+					var user_info = {user_id : u_id, display_name: d_name, account_type: acc_type};
+					array.push(user_info);
+				});
+			}
+			resolve(array);
+		});
+	});
+}
 
+function get_associated_associate(connection, user_id){
+	return new Promise(function(resolve, reject){
+
+	});
+}
+
+function get_associated_clients(connection, user_id){
+	return new Promise(function(resolve, reject){
+
+	});
 }
